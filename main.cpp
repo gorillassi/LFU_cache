6438f1c8b8b02117f78bdf6a8aef42919fd5259d
gorillassi <yasinabuassi@gmail.com>
2024-09-11 17:49:11 +0300

    first working version

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
+#include <iostream>
 
 class LFUCache {
 public:
@@ -10,74 +11,80 @@ public:
         this->capacity = capacity;
     }
     
-    int get(int key) {
-        if(cache.find(key) != cache.end()){
-            incFreq(key);
-            return cache[key];
-        } else{
-            return -1;
-        }
-    }
-    
-    void put(int key, int value) {
-        if(cache.find(key) != cache.end()){
-            cache[key] = value;
+    void put(int value) {
+        if (cache.find(value) != cache.end()) {
             hits++;
-            incFreq(key);
-        } else{
-            if(cache.size() >= capacity){
+            cache[value]++;
+            incFreq(value);
+        } else {
+            if (cache.size() >= capacity) {
                 removeElm();
             } 
-            cache[key] = value;
-            freq[key] = 1;
-            map_of_nums_freq[1].push_back(key);
+            cache[value] = 1; 
+            freq[value] = 1;
+            map_of_nums_freq[1].push_back(value);
             minFreq = 1;
         }
+    }
+
+    int hitsRet() {
+        return hits;
     }
 
 private:
     int capacity = 0;
     int minFreq = 0;
     int hits = 0; 
-    std::unordered_map<int, int> cache;
-    std::unordered_map<int, int> freq;
-    std::unordered_map<int, std::vector<int>> map_of_nums_freq;
 
-    void incFreq(int key){
-        int frequence = freq[key];
-        freq[key]++;
-        map_of_nums_freq[frequence].erase(std::remove(map_of_nums_freq[frequence].begin(), map_of_nums_freq[frequence].end(), key), map_of_nums_freq[frequence].end());
-        map_of_nums_freq[frequence+1].push_back(key);
-        if(map_of_nums_freq[frequence].empty() && frequence == minFreq){
+    std::unordered_map<int, int> cache; 
+    std::unordered_map<int, int> freq; 
+    std::unordered_map<int, std::vector<int>> map_of_nums_freq; 
+
+    void incFreq(int value) {
+        int frequence = freq[value];
+        freq[value]++;
+        
+        if (!map_of_nums_freq[frequence].empty()) {
+            map_of_nums_freq[frequence].erase(std::remove(map_of_nums_freq[frequence].begin(), map_of_nums_freq[frequence].end(), value), map_of_nums_freq[frequence].end());
+        }
+
+        map_of_nums_freq[frequence + 1].push_back(value);
+
+        if (map_of_nums_freq[frequence].empty() && frequence == minFreq) {
             minFreq++;
         }
     }
 
-    void removeElm(){
-        int remEl = map_of_nums_freq[minFreq].front();
-        map_of_nums_freq[minFreq].erase(map_of_nums_freq[minFreq].begin());
-        cache.erase(remEl);
-        freq.erase(remEl);
-    }
-
-    int hitsRet(){
-        return hits;
+    void removeElm() {
+        if (!map_of_nums_freq[minFreq].empty()) {
+            int remEl = map_of_nums_freq[minFreq].front();
+            map_of_nums_freq[minFreq].erase(map_of_nums_freq[minFreq].begin());
+            cache.erase(remEl);
+            freq.erase(remEl);
+        }
     }
 };
 
-int main(){
+int main() {
     int size_of_cache, n, temp  = 0;
     std::cin >> size_of_cache >> n;
 
-    std::unordered_map<int, int> nums;
+    std::unordered_map<int, int> perfNums;
+    std::vector<int> nums(n);
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         std::cin >> temp;
-        nums[temp]++;     
+        perfNums[temp]++;
+        nums[i] = temp;
     }
     
-    LFUCache lfu (size_of_cache);
+    LFUCache lfu(size_of_cache);
+
+    for (int i = 0; i < n; i++) {
+        lfu.put(nums[i]);
+    }
     
+    std::cout << "LFU: " << lfu.hitsRet() << "\n";
+
     return 0;
 }
