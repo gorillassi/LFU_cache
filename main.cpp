75805a89f20dff5752d1a0a550a7cc8c0c5cfcab
gorillassi <yasinabuassi@gmail.com>
2024-09-12 23:38:57 +0300

    split the program into files

--- main.cpp
+++ main.cpp
@@ -1,127 +1,7 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
-
-
-class LFUCache {
-public:
-    LFUCache(int capacity) : capacity(capacity), minFreq(0), hits(0) {}
-
-    void get(int value) {
-        if (cache.find(value) != cache.end()){
-            hits++; 
-            incFreq(value);
-        } else{
-            put(value);
-        }
-    }
-
-    void put(int value) {
-        if (capacity == 0){
-            
-        }
-        else {
-            if (cache.size() >= capacity) {
-                removeElm();
-            }
-            cache[value] = 1;
-            freq[value] = 1;
-            map_of_nums_freq[1].push_back(value);
-            minFreq = 1;
-        }
-    }
-
-    int hitsRet() {
-        return hits;
-    }
-
-private:
-    int capacity;
-    int minFreq;
-    int hits;
-
-    std::unordered_map<int, int> cache; 
-    std::unordered_map<int, int> freq;  
-    std::unordered_map<int, std::vector<int>> map_of_nums_freq; 
-
-    void incFreq(int value) {
-        int currentFreq = freq[value];
-        freq[value]++;
-
-        map_of_nums_freq[currentFreq].erase(std::remove(map_of_nums_freq[currentFreq].begin(),
-                                                        map_of_nums_freq[currentFreq].end(), value), 
-                                                        map_of_nums_freq[currentFreq].end());
-
-        map_of_nums_freq[currentFreq+1].push_back(currentFreq);
-
-        if(map_of_nums_freq[currentFreq].empty() && currentFreq == minFreq){
-            minFreq++;
-        }
-    }
-
-    void removeElm() {
-        if (!map_of_nums_freq[minFreq].empty()) {
-            int remEl = map_of_nums_freq[minFreq].front();
-            map_of_nums_freq[minFreq].erase(map_of_nums_freq[minFreq].begin());
-            cache.erase(remEl);
-            freq.erase(remEl);
-        }
-    }
-};
-
-class IdealCache{
-public:
-    IdealCache(int capacity, std::vector<int>& futureNums) : capacity(capacity), futureNums(futureNums) {}
-
-    void get(int value){
-        if(cache.find(value) != cache.end()){
-            hits++;
-        }else{
-            if(capacity == 0){
-
-            }else{
-                if(cache.size() >= capacity){
-                    removeElm(cache, map_of_freq);
-                }
-                cache[value]++;
-            }
-        }
-    }
-
-    int hitsRet(){
-        return hits;
-    }
-private:
-    int capacity  = 0;
-    int hits = 0;
-
-    std::unordered_map<int, int> cache;
-    std::unordered_map<int, int> map_of_freq;
-    std::vector<int> futureNums;
-
-    void rateFreq(std::unordered_map<int, int> map_of_freq, std::vector<int> futureNums){
-        for(const auto num: futureNums){
-            map_of_freq[num]++;
-        }
-    }
-
-    void removeElm(std::unordered_map<int, int> cache, std::unordered_map<int, int> map_of_freq){
-        int minFreq = 1000;
-        int minNum = 0;
-
-        for(const auto cell: cache){
-            int curFreq = map_of_freq[cell.second];
-            if(curFreq < minFreq){
-                minFreq = curFreq;
-                minNum = cell.first;
-            }
-        }
-
-        cache.erase(minNum);
-    }
-};
+#include "LFUCache.h"
+#include "IdealCache.h"
 
 TEST(LFUTest, Test1) {
     std::vector<int> nums = {1, 2, 1, 2, 1, 2};
@@ -165,7 +45,7 @@ TEST(LFUTest, Test3) {
 }
 
 TEST(LFUTest, Test4) {
-    std::vector<int> nums = {5, 5, 5, 5, 5};
+    std::vector<int> nums = {5, 5, 5, 5, 5, 5};
     LFUCache lfu(1);
     IdealCache idc(1, nums);
 
