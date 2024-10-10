a0c21688b409df78e1deecfbff56de0120e79e6d
gorillassi <yasinabuassi@gmail.com>
2024-10-10 18:43:51 +0300

    corrected the implementation

--- src/IdealCache.h
+++ src/IdealCache.h
@@ -26,3 +26,82 @@ private:
     T removeLeastLikely(T value);
     void put(T value);
 };
+
+template<typename T>
+IdealCache<T>::IdealCache(int capacity, const std::vector<T>& elements) : capacity(capacity), elements(elements), hits(0) {}
+
+template<typename T>
+void IdealCache<T>::fillMapOfUse(std::vector<T>& elements) {
+    for (int i = 0; i < elements.size(); i++) {
+        if (map_of_indexes.find(elements[i]) == map_of_indexes.end()) {
+            map_of_indexes[elements[i]] = std::queue<int>();
+        }
+        map_of_indexes[elements[i]].push(i);
+    }
+}
+
+template<typename T>
+T IdealCache<T>::removeLeastLikely(T value) {
+    int farthest_ind = -1;
+    T farthest_elem;
+
+    for (auto it = cache.begin(); it != cache.end(); it++) {
+        T curr_elem = it->first;
+        std::queue<int> curr_list = map_of_indexes[it->first];
+
+        if (curr_list.empty()) {
+            return curr_elem;
+        }
+
+        if (curr_list.front() > farthest_ind) {
+            farthest_ind = curr_list.front();
+            farthest_elem = curr_elem;
+        }
+    }
+    
+    std::queue<int> new_elem_list = map_of_indexes[value];
+
+    if (new_elem_list.empty() || new_elem_list.front() > farthest_ind) {
+        return value;
+    }
+
+    return farthest_elem;
+}
+
+template<typename T>
+void IdealCache<T>::processStream(std::vector<T>& elements) {
+    fillMapOfUse(elements);
+
+    for (int i = 0; i < elements.size(); i++) {
+        T value = elements[i];
+
+        if (!map_of_indexes[value].empty()) {
+            map_of_indexes[value].pop();
+        }
+        if (cache.find(value) != cache.end()) {
+            hits++;
+            continue;
+        }
+        if (cache.size() < capacity) {
+            put(value);
+        } else {
+            T curr_elem = removeLeastLikely(value);
+
+            if (curr_elem == value) {
+                continue;
+            }
+            cache.erase(curr_elem);
+            put(value);
+        }
+    }  
+}
+
+template<typename T>
+void IdealCache<T>::put(T value) {
+    cache[value] = 0; 
+}
+
+template<typename T>
+int IdealCache<T>::hitret() {
+    return hits;
+}
--- src/LFUCache.h
+++ src/LFUCache.h
@@ -32,3 +32,80 @@ private:
     void delFreqList(int freq);
     void removeElm();
 };
+
+template <typename T>
+LFUCache<T>::LFUCache(int capacity) : capacity(capacity), min_freq(1), hits(0) {}
+
+template <typename T>
+void LFUCache<T>::get(T value) {
+    auto it = cache.find(value);
+    if (it != cache.end()) {
+        hits++;
+        incFreq(it->second); 
+    } else {
+        put(value);
+    }
+}
+
+template <typename T>
+void LFUCache<T>::put(T value) {
+    if (capacity == 0) return;
+
+    auto it = cache.find(value);
+    if (it != cache.end()) {
+        incFreq(it->second); 
+        return;
+    }
+
+    if (cache.size() >= capacity) {
+        removeElm(); 
+    }
+
+    Node<T> newNode(value, 1);
+    map_of_nums_freq[1].push_front(newNode);
+    cache[value] = map_of_nums_freq[1].begin();
+    min_freq = 1; 
+}
+
+template <typename T>
+void LFUCache<T>::incFreq(typename std::list<Node<T>>::iterator node) {
+    int freq = node->freq;
+    T value = node->value;
+
+    auto& old_freq_list = map_of_nums_freq[freq];
+    old_freq_list.erase(node);
+
+    if (old_freq_list.empty()) {
+        delFreqList(freq);
+    }
+
+    int new_freq = freq + 1;
+    map_of_nums_freq[new_freq].emplace_front(value, new_freq);
+    cache[value] = map_of_nums_freq[new_freq].begin();
+}
+
+template <typename T>
+void LFUCache<T>::delFreqList(int freq) {
+    map_of_nums_freq.erase(freq);
+    if (min_freq == freq) {
+        min_freq++;
+    }
+}
+
+template <typename T>
+void LFUCache<T>::removeElm() {
+    auto& least_freq_list = map_of_nums_freq[min_freq];
+    auto node_del = least_freq_list.back();
+    cache.erase(node_del.value);
+
+    least_freq_list.pop_back();
+
+    if (least_freq_list.empty()) {
+        delFreqList(min_freq);
+    }
+}
+
+template <typename T>
+int LFUCache<T>::hitsRet() {
+    return hits; 
+}
